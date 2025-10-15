0946c7918cf82bb45175022c382f1c76e36ae582
Alexander Åberg <alexander_aberg1985@msn.com>
2025-10-15 15:12:34 +0200

    Updated Program to get info from other classes

--- IoTAutomobil/Program.cs
+++ IoTAutomobil/Program.cs
@@ -4,16 +4,18 @@
     {
         static void Main(string[] args)
         {
-        https://api.thingspeak.com/update?api_key=FKN4YZGI0E2HX5SW&field1=0
+        // https://api.thingspeak.com/update?api_key=FKN4YZGI0E2HX5SW&field1=0
 
             while (true)
             {
-                var thingSpeak = new ThingSpeak();
-                int rpm = new Random().Next(800, 6000);
-                int speed = new Random().Next(0, 120);
-                int fuel = new Random().Next(0, 100);
-                int engineTemperature = new Random().Next(80, 100);
-              //  string dtc = "P" + new Random().Next("https://club.autodoc.se/obd-codes/all).ToString"();
+                var sensorData = new SensorData(new Random().Next(800, 6000),
+                    new Random().Next(0, 120),
+                    new Random().Next(0, 100),
+                    new Random().Next(80, 100),
+                    "P" + new Random().Next(https://club.autodoc.se/obd-codes/all).ToString("D4")
+                );
+                var sendDataService = new SendDataService();
+                sendDataService.SendData(sensorData);
 
                 System.Threading.Thread.Sleep(15000);
             }
