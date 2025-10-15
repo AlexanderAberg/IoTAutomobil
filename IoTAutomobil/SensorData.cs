dfd0a7c3d5231dec8bf27b85a139a584d69382dc
Alexander Åberg <alexander_aberg1985@msn.com>
2025-10-15 14:49:26 +0200

    Created class SensorData with code

---
+++ IoTAutomobil/SensorData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTAutomobil
+{
+    internal class SensorData
+    {
+        public SensorData(int rpm, int speed, int fuel, int engineTemperature, string dtc) 
+        {
+            Rpm = rpm;
+            Speed = speed;
+            Fuel = fuel;
+            EngineTemperature = engineTemperature;
+            Dtc = dtc;
+        }
+        public int Rpm { get; set; }
+        public int Speed { get; set; }
+        public int Fuel { get; set; }
+        public int EngineTemperature { get; set; }
+        public string Dtc { get; set; }
+    }
+}
