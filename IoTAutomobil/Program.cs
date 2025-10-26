b67e44f285be328f0fc8546ce71304b628fc4f45
Alexander Åberg <alexander_aberg1985@msn.com>
2025-10-26 22:15:54 +0100

    Removed comment

--- IoTAutomobil/Program.cs
+++ IoTAutomobil/Program.cs
@@ -6,7 +6,6 @@ namespace IoTAutomobil
     {
         static async Task Main(string[] args)
         {
-            // https://api.thingspeak.com/update?api_key=FKN4YZGI0E2HX5SW&field1=0
             // https://club.autodoc.se/obd-codes/all
 
             var car = new Car();
